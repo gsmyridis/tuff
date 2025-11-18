39ca7ce1accf3e59705d994b6ab3b863fd0c6a82
gsmyridis <georgesmyr@icloud.com>
2025-11-18 08:58:05 +0100

    Restructured repo

--- tuff/src/lib.rs
+++ tuff/src/lib.rs
@@ -1,3 +1,13 @@
-pub use tuff_macro::profile_fn;
+#[macro_use]
+pub mod profile;
+pub use profile::{CallSite, ProfileBlock, Profiler};
 
+pub mod report;
+
+pub mod metrics;
+pub use metrics::{Counter, Duration, Frequency, Instant};
+
+// Re-exports
+pub use paste;
 pub use tuff_core::*;
+pub use tuff_macro::profile_fn;
